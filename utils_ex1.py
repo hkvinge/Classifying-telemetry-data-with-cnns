fe04cdeaf46ecee71de76348397e064275dbf75f
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2019-02-07 18:53:34 -0700

    comment cleanup

--- utils_ex1.py
+++ utils_ex1.py
@@ -11,20 +11,20 @@ labels = utils.get_labels('t_post_infection')
 
 mouse_ids = utils.get_labels('mouse_id')
 
-# colors for plot
-colors = pyplot.cm.viridis( np.minimum(1., labels/(5*1440.)) )
-
-# look at the first thirteen points; corresponds to one mouse.
-
+# Look at the first thirteen chunks; corresponds to all data from the first mouse.
 fig,ax = pyplot.subplots(13,1, sharex=True, sharey=True, figsize=(8,8))
+
+# colors for plots; labels==0 is one end of the spectrum; labels>=5 days is the other end.
+colors = pyplot.cm.viridis( np.minimum(1., labels/(5*1440.)) )
 
 for i in range(13):
     ax[i].plot(data[i], lw=2, c=colors[i])
+
+    # prettying the plots a little
     ax[i].set_xticks(np.arange(0,1440+1,1440//4))
     ax[i].xaxis.grid()
 #
 
-#fig.tight_layout()
 fig.suptitle('One-day chunks for mouse %s'%mouse_ids[0], fontsize=18)
 fig.subplots_adjust(top=0.9)
 fig.show()
